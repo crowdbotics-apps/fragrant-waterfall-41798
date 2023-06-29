15d0d01e99706782559c7d14dc2e0b9d4d30dae7
Crowdbotics <team@crowdbotics.com>
2023-06-29 03:43:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fragrant-waterfall-41798",
+  url: "https://fragrant-waterfall-41798.botics.co",
+  api: "https://fragrant-waterfall-41798.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
